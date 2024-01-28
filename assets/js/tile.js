8140b941a2a159faaf9e746f5c89877bfa24379b
Ofir Mikel <159753ofir@gmail.com>
2024-01-29 01:14:04 +0200

    Deployment

--- assets/js/tile.js
+++
@@ -1,8 +0,0 @@
-class Tile {
-  constructor(structure, rotation, imageNumber) {
-    this.structure = structure;
-    this.rotation = rotation;
-    this.imageNumber = imageNumber;
-  }
-}
-export default Tile;
